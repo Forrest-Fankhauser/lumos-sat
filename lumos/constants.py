73f553842ac82643df93af95054bef20cc63e819
Forrest Fankhauser <forrestalan256@gmail.com>
2023-04-08 18:56:49 -0700

    Documentation

--- lumos/constants.py
+++ lumos/constants.py
@@ -1,8 +1,15 @@
 """
-Constants used throughout Lumos
+Fundamental constants used throughout Lumos.
 """
 
-SPEED_OF_LIGHT = 3.0 * 1e8 # meters / second
-WAVELENGTH = 532 * 1e-9     # meters
-EARTH_RADIUS = 6378 * 1000  # meters
-SUN_INTENSITY = 1360 # Watts / meter ^ 2
+#: :math:`3.0 \times 10^{8}`  :math:`\frac{m}{s}`
+SPEED_OF_LIGHT = 3.0 * 1e8
+
+#: :math:`532 \times 10^{-9}`  :math:`m`
+WAVELENGTH = 532 * 1e-9
+
+#: :math:`6378 \times 10^{3}`  :math:`m`
+EARTH_RADIUS = 6378 * 1000
+
+#: :math:`1360`  :math:`\frac{W}{m^2}`
+SUN_INTENSITY = 1360
